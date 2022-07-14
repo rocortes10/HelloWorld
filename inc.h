0582d83eae7a4dccbed556b6aa583cd950b58ab0
RoCor <ro_cortes10@hotmail.com>
2022-07-14 13:37:12 +0200

    forgot the rest

--- inc.h
+++ inc.h
@@ -1,14 +1,12 @@
+#include<iostream>
+using namespace std;
 
 class ClaseBase
 {
     protected:
     int unaVar = 0;
     public:
-    void unMetodo(void)
-    {
-        unaVar++;
-        cout<<"unaVar = "<<unaVar<<endl;
-    }
+    void unMetodo(void);
 };
 
 class ClaseDerivada : public ClaseBase  /* Sintaxis para indicar que ClaseDerivada hereda de ClaseBase */
--- main.cpp
+++ main.cpp
@@ -1,23 +1,7 @@
 #include<iostream>
+#include"inc.h"
 using namespace std;
 
-class ClaseBase
-{
-    protected:
-    int unaVar = 0;
-    public:
-    void unMetodo(void)
-    {
-        unaVar++;
-        cout<<"unaVar = "<<unaVar<<endl;
-    }
-};
-
-class ClaseDerivada : public ClaseBase  /* Sintaxis para indicar que ClaseDerivada hereda de ClaseBase */
-{
-    public:
-    int x=0;
-};
 
 int main()
 {
